b2a728b33d8be60cc7f832e77d95b87a00f0762d
carlossilvajr854 <123024661+carlossilvajr854@users.noreply.github.com>
2023-12-28 17:44:21 -0300

    Refatoração do index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,66 +1,66 @@
-const tagBody = document.querySelector("body");
-const btnMenu = document.getElementById("btn-menu");
-const btnFechaMenu = document.getElementById("btn-fecha-menu");
-const divLightbox = document.querySelector(".lightbox");
-const menuNav = document.querySelector("nav.menu");
 const linksMenu = document.querySelectorAll(".menu ul li a");
-const paginaAtual = document.querySelector(".menu ul li a.pagina-atual");
 
-// Função que exibe ou oculta o menu e seus respectivos botões
-function controlesMenu() {
-    btnMenu.classList.toggle("hidden");
-    btnFechaMenu.classList.toggle("hidden");
-    divLightbox.classList.toggle("hidden");
-    menuNav.classList.toggle("hidden");
-    if (tagBody.className == "") {
-        btnFechaMenu.classList.add("fecha-menu-lateral");
-    }
-}
-
-btnMenu.addEventListener("click", () => {
-    controlesMenu();
-});
-
-btnFechaMenu.addEventListener("click", () => {
-    controlesMenu();
-});
-
-divLightbox.addEventListener("click", () => {
-    controlesMenu();
-});
+clickControlesMenu("#btn-menu");
+clickControlesMenu("#btn-fecha-menu");
+clickControlesMenu(".lightbox");
 
 // Controle do tamanho da barra do header
 window.addEventListener("scroll", () => {
-    const rolouPagina = window.scrollY;
+    const posicaoY = window.scrollY;
 
     marcaLinkAtivo(definePaginaAtual());
 
-    if (rolouPagina > 48) {
-        tagBody.classList.add("cabecalho-retraido");
-        btnFechaMenu.classList.remove("fecha-menu-lateral");
+    if (posicaoY > 48) {
+        adicionarClasse("body", "cabecalho-retraido");
+        removerClasse("#btn-fecha-menu", "fecha-menu-lateral");
     } else {
-        tagBody.classList.remove("cabecalho-retraido");
-        btnFechaMenu.classList.add("fecha-menu-lateral");
+        removerClasse("body", "cabecalho-retraido");
+        adicionarClasse("#btn-fecha-menu", "fecha-menu-lateral");
     }
 });
 
-// Controle da marcação da página atual
+// Calcula em qual é a página atual do usuário ao rolar a página
 function definePaginaAtual() {
     return Math.trunc(window.scrollY / (window.innerHeight - 48));
+    // O cálculo é feito baseado na posição atual do scroll vertical
+    // dividido pelo tamanho da tela menos 48px que é o tamanho do cabeçalho
 }
 
+// Marca a página atual no menu
 function marcaLinkAtivo(pagina) {
-    removerClasse(paginaAtual, "pagina-atual");
-    adicionarClasse(linksMenu[pagina], "pagina-atual");
+    removerClasse(".menu ul li a.pagina-atual", "pagina-atual");
+    linksMenu[pagina].classList.add("pagina-atual");
 }
 
-// Função genérica para remover classe de elemento
-function removerClasse(elemento, classe) {
-    elemento = document.querySelector("." + classe);
-    elemento.classList.remove(classe);
+// Função que exibe ou oculta o menu e seus respectivos botões
+function controlesMenu() {
+    alternarClasse("#btn-menu", "hidden");
+    alternarClasse("#btn-fecha-menu", "hidden");
+    alternarClasse(".lightbox", "hidden");
+    alternarClasse("nav.menu", "hidden");
+    if (document.querySelector("body").className == "") {
+        adicionarClasse("#btn-fecha-menu", "fecha-menu-lateral");
+    }
+}
+
+// Função genérica que dispara as ações de mostrar ou ocultar os controladores do menu
+function clickControlesMenu(controle) {
+    document.querySelector(controle).addEventListener("click", () => {
+        controlesMenu();
+    });
 }
 
 // Função genérica para adicionar classe a elemento
 function adicionarClasse(elemento, classe) {
-    elemento.classList.add(classe);
+    document.querySelector(elemento).classList.add(classe);
+}
+
+// Função genérica para remover classe de elemento
+function removerClasse(elemento, classe) {
+    document.querySelector(elemento).classList.remove(classe);
+}
+
+// Função genérica para alternar classe de elemento
+function alternarClasse(elemento, classe) {
+    document.querySelector(elemento).classList.toggle(classe);
 }
