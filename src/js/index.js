1d7fc9771b33c97efb78b67a4d4f16bb2a68774f
carlossilvajr854 <123024661+carlossilvajr854@users.noreply.github.com>
2023-12-28 02:13:13 -0300

    header e navegação lateral
    Finalização de todos os efeitos e funcionalidades do header responsivo, botões de controle de menu e navegação lateral com efeito de suavização de scroll entre as páginas

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,20 @@
 const tagBody = document.querySelector("body");
 const btnMenu = document.getElementById("btn-menu");
 const btnFechaMenu = document.getElementById("btn-fecha-menu");
+const divLightbox = document.querySelector(".lightbox");
 const menuNav = document.querySelector("nav.menu");
 const linksMenu = document.querySelectorAll(".menu ul li a");
-
-function removerClasse(elemento, classe) {
-    elemento = document.querySelector("." + classe);
-    elemento.classList.remove(classe);
-}
-
-function adicionarClasse(elemento, classe) {
-    elemento.classList.add(classe);
-}
-
-// Controle do tamanho da barra do header
-window.addEventListener("scroll", () => {
-    const rolouPagina = window.scrollY;
-
-    if (rolouPagina > 0) {
-        tagBody.classList.add("cabecalho-retraido");
-    } else {
-        tagBody.classList.remove("cabecalho-retraido");
-    }
-});
+const paginaAtual = document.querySelector(".menu ul li a.pagina-atual");
 
 // Função que exibe ou oculta o menu e seus respectivos botões
 function controlesMenu() {
     btnMenu.classList.toggle("hidden");
     btnFechaMenu.classList.toggle("hidden");
+    divLightbox.classList.toggle("hidden");
     menuNav.classList.toggle("hidden");
+    if (tagBody.className == "") {
+        btnFechaMenu.classList.add("fecha-menu-lateral");
+    }
 }
 
 btnMenu.addEventListener("click", () => {
@@ -39,10 +25,42 @@ btnFechaMenu.addEventListener("click", () => {
     controlesMenu();
 });
 
-// Controle da marcação da página atual
-linksMenu.forEach((link) => {
-    link.addEventListener("click", () => {
-        removerClasse(link, "pagina-atual");
-        adicionarClasse(link, "pagina-atual");
-    });
+divLightbox.addEventListener("click", () => {
+    controlesMenu();
 });
+
+// Controle do tamanho da barra do header
+window.addEventListener("scroll", () => {
+    const rolouPagina = window.scrollY;
+
+    marcaLinkAtivo(definePaginaAtual());
+
+    if (rolouPagina > 48) {
+        tagBody.classList.add("cabecalho-retraido");
+        btnFechaMenu.classList.remove("fecha-menu-lateral");
+    } else {
+        tagBody.classList.remove("cabecalho-retraido");
+        btnFechaMenu.classList.add("fecha-menu-lateral");
+    }
+});
+
+// Controle da marcação da página atual
+function definePaginaAtual() {
+    return Math.trunc(window.scrollY / (window.innerHeight - 48));
+}
+
+function marcaLinkAtivo(pagina) {
+    removerClasse(paginaAtual, "pagina-atual");
+    adicionarClasse(linksMenu[pagina], "pagina-atual");
+}
+
+// Função genérica para remover classe de elemento
+function removerClasse(elemento, classe) {
+    elemento = document.querySelector("." + classe);
+    elemento.classList.remove(classe);
+}
+
+// Função genérica para adicionar classe a elemento
+function adicionarClasse(elemento, classe) {
+    elemento.classList.add(classe);
+}
